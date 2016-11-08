78580cb981e8b085a8ba1a6190499b721ca6099b
Lucas Firmo <lucas.firmo@gmail.com>
2016-11-08 19:47:51 -0200

    Creating BestBuyAPI router

---
+++ ProductCatalog/BestBuyAPI.swift
@@ -0,0 +1,61 @@
+//
+//  BestBuyAPI.swift
+//  ProductCatalog
+//
+//  Created by Lucas Domene Firmo on 11/8/16.
+//  Copyright © 2016 Domene. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+enum BestBuyAPI: URLRequestConvertible {
+    static let baseURLString = "https://api.bestbuy.com/v1/"
+    static let apiKey = "bd6j9ut3parb7csptftw74b5"
+
+    case ListProducts(page: Int, sort: ListProductsSort)
+    
+    enum ListProductsSort {
+        case DESC;
+        case ASC;
+        var value: String {
+            switch self {
+            case .ASC:
+                return "asc";
+            case .DESC:
+                return "desc";
+            }
+        }
+    }
+    
+    var method: HTTPMethod {
+        switch self {
+        case .ListProducts:
+            return .get
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .ListProducts:
+            return "/products"
+        }
+    }
+    
+    var parameters: [String : Any] {
+        switch self {
+        case .ListProducts(let page, let sort):
+            return ["page" : page, "sort": "salePrice.\(sort.value)", "apiKey": BestBuyAPI.apiKey]
+        }
+    }
+    
+    func asURLRequest() throws -> URLRequest {
+        let url = try BestBuyAPI.baseURLString.asURL()
+        
+        var urlRequest = URLRequest(url: url.appendingPathComponent(path))
+        urlRequest.httpMethod = method.rawValue
+        urlRequest = try URLEncoding.default.encode(urlRequest, with: parameters)
+        
+        return urlRequest
+    }
+}
