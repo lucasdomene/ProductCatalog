3bb1d4669bbfc6c98a04641f82016ca8d87542d1
Lucas Firmo <lucas.firmo@gmail.com>
2016-11-08 19:50:44 -0200

    Adding Product model

---
+++ ProductCatalog/Product.swift
@@ -0,0 +1,18 @@
+//
+//  Product.swift
+//  ProductCatalog
+//
+//  Created by Lucas Domene Firmo on 11/8/16.
+//  Copyright © 2016 Domene. All rights reserved.
+//
+
+import Foundation
+
+class Product {
+    
+    var name: String?
+    var brand: String?
+    var price: Double?
+    var description: String?
+    
+}
