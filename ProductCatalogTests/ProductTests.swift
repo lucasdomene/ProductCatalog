698126bb91caa7dee81316f8dc60ce393c57036a
Lucas Firmo <lucas.firmo@gmail.com>
2016-11-10 16:34:34 -0200

    Adding Product tests

--- ProductCatalogTests/ProductTests.swift
+++ ProductCatalogTests/ProductTests.swift
@@ -7,8 +7,14 @@
 //
 
 import XCTest
+import ObjectMapper
+@testable import ProductCatalog
 
 class ProductTests: XCTestCase {
+    
+    let firstProductJSON: [String: Any] = ["productId": 10, "name": "Test Product", "manufacturer": "Test Brand", "salePrice": Double(100), "shortDescription": "This is a test description", "mediumImage": "Test medium image", "largeImage": "Test large image"]
+    
+    let secondProductJSON: [String: Any] = ["productId": 20, "name": "Test Product", "manufacturer": "Test Brand", "salePrice": Double(100), "shortDescription": "This is a test description", "mediumImage": "Test medium image", "largeImage": "Test large image"]
     
     override func setUp() {
         super.setUp()
@@ -20,15 +26,29 @@ class ProductTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testProductCreation() {
+        if let product = Mapper<Product>().map(JSON: firstProductJSON) {
+            XCTAssertEqual(product.id, 10)
+            XCTAssertEqual(product.name, "Test Product")
+            XCTAssertEqual(product.brand, "Test Brand")
+            XCTAssertEqual(product.price, 100)
+            XCTAssertEqual(product.description, "This is a test description")
+            XCTAssertEqual(product.thumbnailImage, "Test medium image")
+            XCTAssertEqual(product.largeImage, "Test large image")
+        } else {
+            XCTFail("Could not create product")
+        }
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testEqualProducts() {
+        if let firstProduct = Mapper<Product>().map(JSON: firstProductJSON) {
+            XCTAssertTrue(firstProduct == firstProduct)
+        }
+    }
+    
+    func testDifferentProducts() {
+        if let firstProduct = Mapper<Product>().map(JSON: firstProductJSON), let secondProduct = Mapper<Product>().map(JSON: secondProductJSON) {
+            XCTAssertFalse(firstProduct == secondProduct)
         }
     }
     
